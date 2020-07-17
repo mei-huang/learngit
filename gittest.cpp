2313462337551a5547fc069df44ad7e3cb6886a8
mei-huang <huang_meia@163.com>
2020-07-17 15:09:42 +0800

    modified gittest.cpp to branch dev

--- gittest.cpp
+++ gittest.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 int main(){	
 	int a=1,b=2;
-	cout<<"a的值= "<<endl;
-	cout<<"b的值= "<<endl;
+	cout<<"a的值= "<<a<<endl;
+	cout<<"b的值= "<<b<<endl;
 	double c=3;
-	cout<<"c的值= "<<endl;
+	cout<<"c的值= "<<c<<endl;
 	return 0;
 
 }
